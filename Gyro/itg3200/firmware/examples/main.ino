b7f4d34705c84307ff08020240d05da8ea07db17
Mihai Galos <mihaigalos@yahoo.com>
2016-11-05 14:40:28 +0100

    Updated copyright, adding spark.json for Particle compatibility

--- Gyro/itg3200/firmware/examples/main.ino
+++ Gyro/itg3200/firmware/examples/main.ino
@@ -1,19 +1,19 @@
 /*****************************************************************************
 * 
-* This file is part of Drivers-Gyro.
+* This file is part of Drivers-Gyro-ITG3200.
 * 
-* Drivers-Gyro is free software: you can redistribute it and/or modify
+* Drivers-Gyro-ITG3200 is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 * 
-* Drivers-Gyro is distributed in the hope that it will be useful,
+* Drivers-Gyro-ITG3200 is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
 * 
 * You should have received a copy of the GNU General Public License
-* along with Drivers-Gyro.  If not, see <http://www.gnu.org/licenses/>.
+* along with Drivers-Gyro-ITG3200.  If not, see <http://www.gnu.org/licenses/>.
 *
 ******************************************************************************/
 // This #include statement was automatically added by the Particle IDE.
