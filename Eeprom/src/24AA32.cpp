b6129fe4740116fd9ea940f38d65288370cb38f2
Mihai Galos <mihaigalos@yahoo.com>
2016-11-20 19:35:44 +0100

    adding license to sources.

--- Eeprom/src/24AA32.cpp
+++ Eeprom/src/24AA32.cpp
@@ -1,3 +1,21 @@
+/*****************************************************************************
+* 
+* This file is part of E2PROM.
+* 
+* E2PROM is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* E2PROM is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with E2PROM.  If not, see <http://www.gnu.org/licenses/>.
+*
+******************************************************************************/
 #include "application.h"
 #include "e2prom.h"
 
