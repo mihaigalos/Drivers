40bcd9d63990ff96df051b634105c477675eef24
Mihai Galos <mihaigalos@gmail.com>
2018-03-27 20:33:10 +0200

    Add softwareUSB support

---
+++ AVR/SoftwareUSB/README.txt
@@ -0,0 +1,13 @@
+This is the source code package for AVR ATtiny USB Tutorial by Joonas Pihlajamaa, published at Code and Life blog, http://codeandlife.com:
+
+http://codeandlife.com/2012/01/22/avr-attiny-usb-tutorial-part-1/
+http://codeandlife.com/2012/01/25/avr-attiny-usb-tutorial-part-2/
+http://codeandlife.com/2012/01/29/avr-attiny-usb-tutorial-part-3/
+http://codeandlife.com/2012/02/04/avr-attiny-usb-tutorial-part-4/
+ 
+
+The subfolders contain parts of libusb-win32 available at http://sourceforge.net/apps/trac/libusb-win32/wiki and V-USB library available at http://www.obdev.at/avrusb/ and their contents are copyrighted by their respective authors. My productions in the root folder are published under GNU GPL v3 (see License.txt).
+
+I hope you have fun with this project!
+
+- Joonas Pihlajamaa
---
+++ AVR/SoftwareUSB/main.c
@@ -0,0 +1,117 @@
+/**
+ * Project: AVR ATtiny USB Tutorial at http://codeandlife.com/
+ * Author: Joonas Pihlajamaa, joonas.pihlajamaa@iki.fi
+ * Inspired by V-USB example code by Christian Starkjohann
+ * Copyright: (C) 2012 by Joonas Pihlajamaa
+ * License: GNU GPL v3 (see License.txt)
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+
+#include "usbdrv.h"
+
+#define F_CPU 16000000L
+#include <util/delay.h>
+
+#include <stdint.h>
+
+#define USB_LED_OFF 0
+#define USB_LED_ON  1
+#define USB_DATA_OUT 2
+#define USB_DATA_WRITE 3
+#define USB_DATA_IN 4
+
+#define columnCount 31
+#define bytesPercolumn 8
+
+static uchar replyBuf[columnCount * bytesPercolumn] = "Hello, USB!";
+static uchar dataReceived = 0, dataLength = 0; // for USB_DATA_IN
+
+static uint16_t offset = 0;
+
+void fillBufferFromFlash() {
+
+	for (uint16_t i = 0; i < sizeof(replyBuf); ++i) {
+		replyBuf[i] = pgm_read_byte_near(i + offset);
+	}
+	offset += sizeof(replyBuf);
+
+}
+
+// this gets called when custom control message is received
+USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
+	usbRequest_t *rq = (void *) data; // cast data to correct type
+
+	switch (rq->bRequest) { // custom command is in the bRequest field
+	case USB_LED_ON:
+		PORTD &= ~(1 << 0) & ~(1 << 1); // turn LED on
+		return 0;
+	case USB_LED_OFF:
+		PORTD |= (1 << 0) | (1 << 1); // turn LED off
+		return 0;
+	case USB_DATA_OUT: // send data to PC
+		fillBufferFromFlash();
+		usbMsgPtr = replyBuf;
+		return sizeof(replyBuf);
+	case USB_DATA_WRITE: // modify reply buffer
+		replyBuf[7] = rq->wValue.bytes[0];
+		replyBuf[8] = rq->wValue.bytes[1];
+		replyBuf[9] = rq->wIndex.bytes[0];
+		replyBuf[10] = rq->wIndex.bytes[1];
+		return 0;
+	case USB_DATA_IN: // receive data from PC
+		dataLength = (uchar) rq->wLength.word;
+		dataReceived = 0;
+
+		if (dataLength > sizeof(replyBuf)) // limit to buffer size
+			dataLength = sizeof(replyBuf);
+
+		return USB_NO_MSG; // usbFunctionWrite will be called now
+	}
+
+	return 0; // should not get here
+}
+
+// This gets called when data is sent from PC to the device
+USB_PUBLIC uchar usbFunctionWrite(uchar *data, uchar len) {
+	uchar i;
+
+	for (i = 0; dataReceived < dataLength && i < len; i++, dataReceived++)
+		replyBuf[dataReceived] = data[i];
+
+	return (dataReceived == dataLength); // 1 if we received it all, 0 if not
+}
+
+void USB_INTR_VECTOR(void);
+
+int main() {
+	uchar i;
+
+	DDRD = (1 << 1 | 1 << 0); // PB0 as output
+
+//wdt_enable (WDTO_1S); // enable 1s watchdog timer
+
+	usbInit();
+
+	usbDeviceDisconnect(); // enforce re-enumeration
+	for (i = 0; i < 250; i++) { // wait 500 ms
+		wdt_reset(); // keep the watchdog happy
+		_delay_ms(2);
+	}
+	usbDeviceConnect();
+
+	sei(); // Enable interrupts after re-enumeration
+
+	while (1) {
+		if (USB_INTR_PENDING & (1 << USB_INTR_PENDING_BIT)) {
+			USB_INTR_VECTOR();
+			USB_INTR_PENDING = 1 << USB_INTR_PENDING_BIT; // Clear int pending, in case timeout occured during SYNC                     
+		}
+
+		wdt_reset(); // keep the watchdog happy
+		usbPoll();
+	}
+
+	return 0;
+}
