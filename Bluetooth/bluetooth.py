5bb18a7beef50b7a727a33388541319a32af5993
Mihai Galos <mihaigalos@gmail.com>
2018-11-07 20:15:21 +0100

    Add delegate getter, runonce

--- Bluetooth/bluetooth.py
+++ Bluetooth/bluetooth.py
@@ -2,15 +2,20 @@
 # Gread Android app for debugging : LightBlue Explorer.
 
 import bluepy.btle as btle
-import binascii
-import struct
 
 class MyDelegate(btle.DefaultDelegate):
     def __init__(self):
         btle.DefaultDelegate.__init__(self)
+        self.data = ""
 
     def handleNotification(self, cHandle, data):
-        print("A notification was received: %s" %data)
+        self.data = data
+
+    def getData(self):
+        return self.data
+
+    def clearData(self):
+        self.data =""
 
 class BluetoothModule:
     def __init__(self, address):
@@ -21,17 +26,21 @@ class BluetoothModule:
         characteristic = service.getCharacteristics()[2]
         characteristic.write(bytes(string_to_write), True)
 
-    def read(self):
-        self.peripheral.setDelegate( MyDelegate() )
+    def read(self, once=True):
+        delegate = MyDelegate()
+        self.peripheral.setDelegate( delegate )
         svc = self.peripheral.getServiceByUUID( "0000ffe0-0000-1000-8000-00805f9b34fb")
         ch = svc.getCharacteristics()[0]
-        print(ch.valHandle)
+
         self.peripheral.writeCharacteristic(ch.valHandle+1, "\x01\x00")
 
         while True:
+            data = delegate.getData()
+            if once and len(data) > 0:
+                return data
             if self.peripheral.waitForNotifications(1.0):
-                continue
-            print("Waiting...")
+                    continue
+
 
 def try_me():
     jdy16 = BluetoothModule("3C:A5:39:90:BB:B1")
