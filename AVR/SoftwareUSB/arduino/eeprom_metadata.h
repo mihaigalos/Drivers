18765c48cb8dd225ecfb10eabd34d12d180de48e
Mihai Galos <mihaigalos@gmail.com>
2020-04-10 20:59:15 +0200

    Reformat, improve naming

--- AVR/SoftwareUSB/arduino/eeprom_metadata.h
+++ AVR/SoftwareUSB/arduino/eeprom_metadata.h
@@ -2,15 +2,16 @@
 
 static constexpr auto kEEPROMMetadataAddress = 0;
 
-typedef struct {
-  uint8_t timezone_sign : 1; // 1=+, 0=-
-  uint8_t utc_offset : 4;    // +/- raw offset to UTC time, not accounting for
-                             // daylight saving
+typedef struct
+{
+  uint8_t timezone_sign : 1;             // 1=+, 0=-
+  uint8_t utc_offset : 4;                // +/- raw offset to UTC time, not accounting for
+                                         // daylight saving
   uint8_t is_daylight_saving_active : 1; // 0=no, 1=yes, if date is > last
                                          // sunday of March and < Last sunday of
                                          // October
-  uint8_t is_china_time : 1; // neccessary, because china has no daylight
-                             // saving, so depending on
+  uint8_t is_china_time : 1;             // neccessary, because china has no daylight
+                                         // saving, so depending on
   // is_daylight_saving_active, diff might be +8h or +7h.
   uint8_t unused : 1;
 } TimeZoneInfo;
@@ -20,54 +21,66 @@ typedef union {
   uint8_t u_timezone_info;
 } UTimeZoneInfo;
 
-typedef struct {
+using SoftwareLastUpdatedTimestamp = uint8_t[4];
+
+typedef struct
+{
   uint8_t major : 3;
   uint8_t minor : 3;
   uint8_t patch : 2;
 } VersionInfo;
+
+using HardwareVersion = VersionInfo;
+using SoftwareVersion = VersionInfo;
+using MetadataVersion = VersionInfo;
 
 typedef union {
   VersionInfo s_version_info;
   uint8_t u_version_info;
 } UVersionInfo;
 
-enum class DeviceType : uint8_t { Unknown, DotPhat, DotStix, DotShine };
+enum class DeviceType : uint8_t
+{
+  Unknown,
+  DotPhat,
+  DotStix,
+  DotShine
+};
 
 using UnitName = char[8];
 
-enum class InstalledCapacity : uint8_t{
+enum class InstalledCapacity : uint8_t
+{
   None,
-  One_1000uF,
-  Two_1000uF,
-  Theree_1000uF,
-  One_1F,
-  One_3_3F,
-  Reserved2,
+  Fahrads_1,
   Invalid
 };
 
 using HarvestingCapability = uint8_t;
 
-typedef struct {
+typedef struct
+{
   InstalledCapacity installed_capacity : 3;
   HarvestingCapability harversting_capability_x_15mW : 5;
-}EnergyInfo;
+} EnergyInfo;
 
 typedef union {
   EnergyInfo s_energy_info;
   uint8_t u_energy_info;
-}UEnergyInfo;
+} UEnergyInfo;
 
 using PositionDegrees = float;
 
-typedef struct  {
+typedef struct
+{
   PositionDegrees latitude;
   PositionDegrees longitude;
-}GPSPosition;
+} GPSPosition;
 
 using PositionAdditionalInfo = char[16];
 
-typedef struct {
+typedef struct
+{
   uint8_t temperature_sensor : 1;
   uint8_t ultraviolet_sensor : 1;
   uint8_t eeprom : 1;
@@ -77,21 +90,23 @@ typedef struct {
   uint8_t high_precision_time_reference : 1;
   uint8_t reset_pushbutton : 1;
   uint8_t act_pushbutton : 1;
-}InstalledDevices;
+} InstalledDevices;
 
-typedef union{
+typedef union {
   InstalledDevices s_installed_devices;
   uint8_t u_installed_devices;
-}UInstalledDevices;
+} UInstalledDevices;
 
-typedef struct {
+typedef struct
+{
   uint8_t usb : 1;
   uint8_t external_antenna : 1;
   uint8_t antenna_calibration : 1;
   uint8_t solar_panel : 1;
-}InstalledDevices2;
+} InstalledDevices2;
 
-typedef struct {
+typedef struct
+{
   uint8_t usb_power : 1;
   uint8_t outA : 1;
   uint8_t outB : 1;
@@ -101,19 +116,21 @@ typedef struct {
   uint8_t tx : 1;
   uint8_t rx : 1;
   uint8_t reserved : 1;
-}InstalledLeds;
+} InstalledLeds;
 
-typedef struct SEEPROMMetadata {
-  VersionInfo metadata_version_info;
+typedef struct SEEPROMMetadata
+{
+  MetadataVersion metadata_version_info;
   DeviceType device_type;
 
-  VersionInfo software_version;
+  SoftwareVersion software_version;
   TimeZoneInfo sofware_timezone_info;
   uint8_t software_version_last_updated_timestamp[4];
 
-  VersionInfo hardware_version;
+  HardwareVersion hardware_version;
   TimeZoneInfo hardware_timezone_info;
-  uint8_t hardware_version_timestamp[4];
+  uint8_t hardware_version_timestamp[4]; // hardware_version_timestamp : add 8 hours to the PCB manufactureing time
+                                         // in China
 
   UnitName unit_name;
   UEnergyInfo energy_info;
@@ -125,16 +142,19 @@ typedef struct SEEPROMMetadata {
 
   InstalledLeds installed_leds;
 
-  bool operator!=(const struct SEEPROMMetadata &rhs) const {
+  bool operator!=(const struct SEEPROMMetadata &rhs) const
+  {
     const uint8_t *p = reinterpret_cast<const uint8_t *>(&rhs);
-    for (uint16_t i = 0; i < sizeof(*this); ++i) {
+    for (uint16_t i = 0; i < sizeof(*this); ++i)
+    {
       if (*(p + i) != *(reinterpret_cast<const uint8_t *>(this) + i))
         return true;
     }
     return false;
   }
 
-  bool is_valid_sw_timestamp() {
+  bool is_valid_sw_timestamp()
+  {
     return 0xFF != software_version_last_updated_timestamp[0] &&
            0x00 != software_version_last_updated_timestamp[0] &&
            0xFF != software_version_last_updated_timestamp[1] &&
@@ -145,9 +165,11 @@ typedef struct SEEPROMMetadata {
            0x00 != software_version_last_updated_timestamp[3];
   }
 #ifdef ARDUINO
-  String to_hex() {
+  String to_hex()
+  {
     String result;
-    for (uint8_t i = 0; i < sizeof(*this); ++i) {
+    for (uint8_t i = 0; i < sizeof(*this); ++i)
+    {
       int i_element = *(reinterpret_cast<uint8_t *>(this) + i);
       String s_element{i_element, HEX};
 
