344b86f03c6087311580ea5017bc58bae7f487b3
Mihai Galos <mihaigalos@gmail.com>
2020-12-05 22:08:11 +0100

    Fix parameter order

--- AVR/Encryption/main.cpp
+++ AVR/Encryption/main.cpp
@@ -11,13 +11,13 @@ int main()
     uint8_t key[] = {0x45, 0x74, 0x32, 0x11, 0x98, 0x94, 0xAB, 0xCF, 0x90, 0xAE, 0xBA, 0xDC, 0x06, 0x16, 0x81, 0x95};
 
     secret_data[0] = PINB;
-    tea.encrypt(rounds, secret_data, key);
+    tea.encrypt(rounds, key, secret_data);
     if (secret_data[0] & (PINB))
     {
         PORTC = 0x05;
     }
     secret_data[1] = PINB & (1 << 2);
-    tea.decrypt(rounds, secret_data, key);
+    tea.decrypt(rounds, key, secret_data);
     if (secret_data[0] & (PINB))
     {
         PORTC = 0x50;
